c15bdf9dc6ede2cde6c607199836c365dbd4f3dc
oliverfree <2375828407@qq.com>
2017-09-02 22:57:11 +0800

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -6,10 +6,10 @@ GIT_REPO = "https://github.com/oliverfree/blogproject.git"
 env.user = 'daniel' 
 env.password = 'Dengyu8403'
 
-# 填写你自己的主机对应的域名
+
 env.hosts = ['doraemon.red']
 
-# 一般情况下为 22 端口，如果非 22 端口请查看你的主机服务提供商提供的信息
+
 env.port = '22'
 
 
