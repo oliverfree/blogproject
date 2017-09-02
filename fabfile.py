42f3c1fb59e97d276cd4a8d72d21bc0d76c0dbd2
oliverfree <2375828407@qq.com>
2017-09-03 07:38:08 +0800

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,27 +1,29 @@
+#coding=utf-8
+
 from fabric.api import env, run
 from fabric.operations import sudo
 
-GIT_REPO = "https://github.com/oliverfree/blogproject.git" 
+GIT_REPO = "you git repository" ① 
 
-env.user = 'daniel' 
-env.password = 'Dengyu8403'
+env.user = 'you host username' ②
+env.password = 'you host password'
 
+# 填写你自己的主机对应的域名
+env.hosts = ['demo.zmrenwu.com']
 
-env.hosts = ['doraemon.red']
-
-
+# 一般情况下为 22 端口，如果非 22 端口请查看你的主机服务提供商提供的信息
 env.port = '22'
 
 
 def deploy():
-    source_folder = '/home/daniel/sites/doraemon.red/blogproject' 
+    source_folder = '/home/yangxg/sites/zmrenwu.com/django-blog-tutorial' ③
 
-    run('cd %s && git pull' % source_folder) 
+    run('cd %s && git pull' % source_folder) ④
     run("""
         cd {} &&
         ../env/bin/pip install -r requirements.txt &&
         ../env/bin/python3 manage.py collectstatic --noinput &&
         ../env/bin/python3 manage.py migrate
-        """.format(source_folder)) 
-    sudo('restart gunicorn-doraemon.red') 
+        """.format(source_folder)) ⑤ 
+    sudo('restart gunicorn-demo.zmrenwu.com') ⑥
     sudo('service nginx reload')
